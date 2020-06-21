52a3441bbfa18233e1a27ca78495973ec0c5fe32
afellman <andrewf@mbshighway.com>
2020-06-20 23:48:36 -0400

    push

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const fs = require('fs');
 const getMP3Duration = require('get-mp3-duration')
 var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
-const port = process.argv[2] || 80
+const port = process.argv[2] || 80;
+const fileUpload = require('express-fileupload');
 
 // require('routes')(app, path)
 app.set('view engine', 'ejs');
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // use res.render to load up an ejs view file
 app.use(express.static(__dirname + '/public'))
-
+app.use(fileUpload());
 
 // var transporter = nodemailer.createTransport(smtpTransport({
 //     service: 'Gmail',
@@ -48,22 +49,24 @@ app.get('/about', function (req, res) {
     res.render('pages/about');
 });
 
-app.get('/faq', function (req, res) {
-    res.render('pages/faq');
+app.get('/backend', function (req, res) {
+    res.render('pages/backend');
 });
 
 
-app.get('/links', function (req, res) {
-    res.render('pages/links');
-});
+// app.get('/faq', function (req, res) {
+//     res.render('pages/faq');
+// });
 
+// app.get('/links', function (req, res) {
+//     res.render('pages/links');
+// });
 
 
 
 // API Routes
 app.get("/tracks", (req, res) => {
     fs.readFile(__dirname + "/tracks.json", "utf-8", (err, items) => {
-        console.log(items)
         const itemsParsed = JSON.parse(items);
         attachDuration(itemsParsed).then((itemsWithData) => {
             console.log(itemsWithData)
@@ -73,27 +76,39 @@ app.get("/tracks", (req, res) => {
 
 });
 
-app.post('/contact-submit', (req, res) => {
-    var mailOptions = {
-        from: 'hummoscontactus@gmail.com',
-        to: 'hummos1985@gmail.com',
-        cc: 'andrewfellman@abrahamsnatural.com',
-        subject: req.body.subject,
-        text: req.body.message,
-        replyTo: req.body.email,
+app.post("/api/newSongText", (req, res) => {
+    console.log(req.body);
+});
 
-    };
+app.post("/api/newSongAudio", (req, res) => {
+    console.log(req.files);
+});
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-    console.log(req.body)
-    res.end('Submitted')
-})
+app.post("/api/newSongImg", (req, res) => {
+    console.log(req.files);
+});
+
+// app.post('/contact-submit', (req, res) => {
+//     var mailOptions = {
+//         from: 'hummoscontactus@gmail.com',
+//         to: 'hummos1985@gmail.com',
+//         cc: 'andrewfellman@abrahamsnatural.com',
+//         subject: req.body.subject,
+//         text: req.body.message,
+//         replyTo: req.body.email,
+
+//     };
+
+//     transporter.sendMail(mailOptions, function (error, info) {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             console.log('Email sent: ' + info.response);
+//         }
+//     });
+//     console.log(req.body)
+//     res.end('Submitted')
+// })
 
 
 function attachDuration(tracks) {
